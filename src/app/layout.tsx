c503499b915e5ce6c0af1e4cebb06001c2e9d650
Pridesd <whwotjr98@naver.com>
2025-03-13 20:43:14 +0900

    feat: wrap head elements in <head> tag for improved HTML structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,16 +10,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <title>가이드런 프로젝트</title>
-      <meta
-        name="description"
-        content="시각장애러너와 가이드러너가 함께 만들어가는 가이드런 프로젝트입니다. 함께 달려요!"
-      />
-      <link rel="icon" href={Logo.src} />
-      <link rel="apple-touch-icon" href={Logo.src} />
-      <meta property="og:title" content="가이드런프로젝트" />
-      <meta property="og:description" content="함께 달려요!" />
-      <meta property="og:image" content={OGImage.src} />
+      <head>
+        <title>가이드런 프로젝트</title>
+        <meta
+          name="description"
+          content="시각장애러너와 가이드러너가 함께 만들어가는 가이드런 프로젝트입니다. 함께 달려요!"
+        />
+        <link rel="icon" href={Logo.src} />
+        <link rel="apple-touch-icon" href={Logo.src} />
+        <meta property="og:title" content="가이드런프로젝트" />
+        <meta property="og:description" content="함께 달려요!" />
+        <meta property="og:image" content={OGImage.src} />
+      </head>
       <body>{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
