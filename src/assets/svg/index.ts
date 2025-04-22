1c9461126d1baf42758f23ee32281d9c415596d2
Pridesd <whwotjr98@naver.com>
2025-04-22 21:48:44 +0900

    fix: add TextLogoWhite icon and update Copyright component for improved responsiveness and layout

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,12 +1,13 @@
-import ArrowRightSmall from "./arrow-right-small.svg";
-import ArrowRightUp from "./arrow-right-up.svg";
-import ArrowRightWhite from "./arrow-right-white.svg";
-import ArrowRight from "./arrow-right.svg";
-import Clear from "./clear.svg";
-import GuiderunCopyright from "./guiderun-copyright.svg";
-import Line from "./line.svg";
-import TextLogo from "./text-logo.svg";
-import Workout from "./workout.svg";
+import ArrowRightSmall from "./arrow-right-small.svg"
+import ArrowRightUp from "./arrow-right-up.svg"
+import ArrowRightWhite from "./arrow-right-white.svg"
+import ArrowRight from "./arrow-right.svg"
+import Clear from "./clear.svg"
+import GuiderunCopyright from "./guiderun-copyright.svg"
+import Line from "./line.svg"
+import TextLogoWhite from "./text-logo-white.svg"
+import TextLogo from "./text-logo.svg"
+import Workout from "./workout.svg"
 
 export const Icons = {
   ArrowRightSmall,
@@ -16,9 +17,10 @@ export const Icons = {
   Clear,
   GuiderunCopyright,
   Line,
+  TextLogoWhite,
   TextLogo,
   Workout,
-};
+}
 
-export type IconType = typeof Icons;
-export type IconKey = keyof IconType;
+export type IconType = typeof Icons
+export type IconKey = keyof IconType
--- src/domains/home/mobile/Copyright.tsx
+++ src/domains/home/mobile/Copyright.tsx
@@ -2,11 +2,20 @@ import { Icon } from "@/components/shared"
 
 export const Copyright = () => {
   return (
-    <div className="box-border h-[100px] w-full bg-black px-5 py-6">
+    <div className="relative box-border h-[100px] w-full bg-black px-5 py-6 md:flex md:h-[80px] md:items-center">
       <Icon
+        className="md:hidden"
         icon="GuiderunCopyright"
         alt="(c)Guiderun Project All Right Reserved."
       />
+      <Icon
+        className="hidden md:block"
+        icon="TextLogoWhite"
+        alt="가이드런 프로젝트"
+      />
+      <span className="absolute top-1/2 left-1/2 hidden -translate-x-1/2 -translate-y-1/2 text-white md:inline">
+        (c)Guiderun Project All Right Reserved.
+      </span>
     </div>
   )
 }
