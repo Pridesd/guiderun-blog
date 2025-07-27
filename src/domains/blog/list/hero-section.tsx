a8b929fad799d0496edb779c76eae0b1c7ec51f5
Pridesd <whwotjr98@naver.com>
2025-07-27 17:28:17 +0900

    feat: set Responsive design

--- src/domains/blog/list/hero-section.tsx
+++ src/domains/blog/list/hero-section.tsx
@@ -3,17 +3,19 @@ import Image from "next/image"
 
 export const HeroSection = () => {
   return (
-    <div className="flex flex-col items-center gap-[1.75rem] pt-[6.5rem] pb-[6.25rem] md:gap-[2.75rem]">
+    <div className="flex flex-col items-center gap-[1.75rem] pt-[5.5rem] pb-[5.25rem] md:gap-[2.75rem] md:pt-[6.5rem] md:pb-[6.25rem]">
       <Image
         src={RunningImage}
         alt="끈을 잡고 뛰고 있는 두 명의 가이드러너와 한 명의 시각장애러너 뒷 모습"
         className="w-full rounded-3xl"
       />
       <h1 className="flex flex-col items-center gap-3">
-        <span className="text-[1.625rem] font-bold">
+        <span className="text-xl font-bold lg:text-[1.625rem]">
           우리는 보이지 않는 길을 함께 달려요
         </span>
-        <span className="text-[#868b94]">가이드런 프로젝트 블로그</span>
+        <span className="text-sm lg:text-[#868b94]">
+          가이드런 프로젝트 블로그
+        </span>
       </h1>
     </div>
   )
