4e2beae39a03bb5ee1cc762f01d1939d194e66bc
Pridesd <whwotjr98@naver.com>
2025-07-27 18:40:03 +0900

    feat: set current border

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -4,8 +4,27 @@ import { Icon } from "@/components/shared"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
+const NAVIGATIONS: { path: string; label: string }[] = [
+  {
+    path: "/",
+    label: "Home",
+  },
+  {
+    path: "/blog",
+    label: "Blog",
+  },
+]
+
 export const Header = () => {
   const pathname = usePathname()
+
+  const checkCurrentPath = (path: string, checkStartWith: boolean = false) => {
+    if (checkStartWith) {
+      return pathname.startsWith(path)
+    }
+
+    return pathname === path
+  }
 
   return (
     <header className="fixed top-0 right-0 left-0 z-10 flex items-center justify-between bg-[#fff] p-5">
@@ -17,18 +36,15 @@ export const Header = () => {
         />
       </Link>
       <nav className="flex items-center justify-end gap-3 lg:gap-[3.75rem]">
-        <Link
-          aria-current={pathname === "/" ? "page" : undefined}
-          href="/"
-          className="font-medium lg:text-[1.25rem]">
-          Home
-        </Link>
-        <Link
-          aria-current={pathname.startsWith("/blog") ? "page" : undefined}
-          href="/blog"
-          className="font-medium lg:text-[1.25rem]">
-          Blog
-        </Link>
+        {NAVIGATIONS.map((navi) => (
+          <Link
+            key={navi.label}
+            aria-current={checkCurrentPath(navi.path) ? "page" : undefined}
+            href="/"
+            className={`border-b-2 font-medium lg:text-[1.25rem] ${checkCurrentPath(navi.path) ? "border-b-black" : "border-b-[#fff]"} hover:border-b-black`}>
+            {navi.label}
+          </Link>
+        ))}
       </nav>
     </header>
   )
--- src/domains/Landing/Header.tsx
+++ src/domains/Landing/Header.tsx
@@ -125,7 +125,10 @@ export const Header = () => {
         </StaggerAnimation>
       </div>
       <nav className="fixed top-4 right-7 z-40 flex items-center justify-end gap-3 md:top-3 lg:gap-[3.75rem]">
-        <Link aria-current href="/" className="font-medium lg:text-[1.25rem]">
+        <Link
+          aria-current
+          href="/"
+          className="border-b-2 font-medium lg:text-[1.25rem]">
           Home
         </Link>
         <Link href="/blog" className="font-medium lg:text-[1.25rem]">
