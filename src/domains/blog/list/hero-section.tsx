b1996dc3a83444722ca27f99dc99efe19be7da58
Pridesd <whwotjr98@naver.com>
2025-07-27 18:27:24 +0900

    chore: change text

--- src/domains/blog/list/hero-section.tsx
+++ src/domains/blog/list/hero-section.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 export const HeroSection = () => {
   return (
-    <div className="flex flex-col items-center gap-[1.75rem] pt-[5.5rem] pb-[5.25rem] md:gap-[2.75rem] md:pt-[6.5rem] md:pb-[6.25rem]">
+    <div className="flex flex-col items-center gap-[1.75rem] pt-[5.5rem] pb-[5.25rem] md:gap-[2.75rem] md:pt-[6.5rem]">
       <Image
         src={RunningImage}
         alt="끈을 잡고 뛰고 있는 두 명의 가이드러너와 한 명의 시각장애러너 뒷 모습"
@@ -14,7 +14,7 @@ export const HeroSection = () => {
           우리는 보이지 않는 길을 함께 달려요
         </span>
         <span className="text-sm lg:text-[#868b94]">
-          가이드런 프로젝트 블로그
+          가이드런 프로젝트 이야기
         </span>
       </h1>
     </div>
