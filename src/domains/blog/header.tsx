bf891b892647e8a0593c0a73c8067ef2c93f91ab
Pridesd <whwotjr98@naver.com>
2025-07-27 16:37:41 +0900

    feat: add Blog Header

---
+++ src/domains/blog/header.tsx
@@ -0,0 +1,27 @@
+import { Icon } from "@/components/shared"
+import Link from "next/link"
+
+export const Header = () => {
+  return (
+    <header className="fixed top-0 right-0 left-0 z-10 flex items-center justify-between bg-[#F2F2F2] p-5">
+      <Link href="/">
+        <Icon
+          icon="TextLogo"
+          alt="가이드런 프로젝트"
+          className="w-[111px] md:w-[222px]"
+        />
+      </Link>
+      <nav className="flex items-center justify-end gap-3 lg:gap-[3.75rem]">
+        <Link href="/" className="font-medium lg:text-[1.25rem]">
+          Home
+        </Link>
+        <Link
+          aria-current
+          href="/blog"
+          className="font-medium lg:text-[1.25rem]">
+          Blog
+        </Link>
+      </nav>
+    </header>
+  )
+}
