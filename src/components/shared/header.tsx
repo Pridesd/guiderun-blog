71dd05652ac238090b6ddbf5cff83fb01c710c1e
Pridesd <whwotjr98@naver.com>
2025-07-27 18:10:06 +0900

    chore: set header

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -1,7 +1,12 @@
+"use client"
+
 import { Icon } from "@/components/shared"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export const Header = () => {
+  const pathname = usePathname()
+
   return (
     <header className="fixed top-0 right-0 left-0 z-10 flex items-center justify-between bg-[#fff] p-5">
       <Link href="/">
@@ -12,11 +17,14 @@ export const Header = () => {
         />
       </Link>
       <nav className="flex items-center justify-end gap-3 lg:gap-[3.75rem]">
-        <Link href="/" className="font-medium lg:text-[1.25rem]">
+        <Link
+          aria-current={pathname === "/" ? "page" : undefined}
+          href="/"
+          className="font-medium lg:text-[1.25rem]">
           Home
         </Link>
         <Link
-          aria-current
+          aria-current={pathname.startsWith("/blog") ? "page" : undefined}
           href="/blog"
           className="font-medium lg:text-[1.25rem]">
           Blog
--- src/domains/blog/index.ts
+++ src/domains/blog/index.ts
@@ -1,1 +1,1 @@
-export * from "./header"
+export * from "./blog-list-layout"
