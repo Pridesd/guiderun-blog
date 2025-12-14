a408646bf844a54b3838a57711e12564ff2e8dc5
Pridesd <whwotjr98@naver.com>
2025-12-14 15:08:53 +0900

    feat: update `GuideRun` component in landing domain

--- src/domains/landing/guide-run.tsx
+++ src/domains/landing/guide-run.tsx
@@ -9,15 +9,21 @@ import GuideRunImage from "@/assets/home/guiderun-background.png"
 export interface GuideRunInfo {
   title: string
   content: string
-  link: string
-  linkLabel: string
+  link: {
+    url: string
+    label: string
+  }[]
 }
 
 const CONTENTS: GuideRunInfo[] = [
   {
     title: "가이드런프로젝트",
-    link: "https://guiderun.notion.site/1be51d244ec342b585e9b78f39ec4628",
-    linkLabel: "가이드런프로젝트에 대하여",
+    link: [
+      {
+        url: "https://guiderun.notion.site/1be51d244ec342b585e9b78f39ec4628",
+        label: "가이드런프로젝트 노션페이지",
+      },
+    ],
     content: `가이드런프로젝트는 달리기를 통해
 장애인과 비장애인이 함께 성취를 이루고,
 그 과정 중에 더불어 살아가는 사회를
@@ -25,23 +31,35 @@ const CONTENTS: GuideRunInfo[] = [
 러닝 커뮤니티 입니다.`,
   },
   {
-    title: "가이드런서비스",
-    link: "https://guiderun.org/intro",
-    linkLabel: "가이드런서비스 바로가기",
+    title: "GrP 서비스",
+    link: [
+      {
+        url: "https://guiderun.org/intro",
+        label: "guiderun.org 가입하기",
+      },
+      {
+        url: "https://about.guiderun.org/blog/how-guiderun-service-started",
+        label: "가이드런서비스에 대하여",
+      },
+    ],
     content: `우리는 시각장애러너와 가이드러너가
 쉽게 파트너를 찾고 뛸 수 있도록,
 어느 곳에도 얽메이지 않고 자유롭게 뛸 수 있도록
 서비스를 기획, 개발하고 있습니다.`,
   },
   {
-    title: "크라우드펀딩",
-    link: "https://happybean.naver.com/fundings/detail/F1677",
-    linkLabel: "최근 크라우드펀딩 구경하기",
-    content: `우리는 프로그램 운영비 마련을 위해
-크라우드펀딩을 진행합니다.
-이렇게 모금과 참여를 분리함으로써,
-커뮤니티의 범위를 확장하고
-프로그램 참가자들의 심적 접근성을 낮춥니다.`,
+    title: "스마트스토어",
+    link: [
+      {
+        url: "https://smartstore.naver.com/guiderunproject",
+        label: "GrP 스마트스토어 바로가기",
+      },
+    ],
+    content: `가이드런프로젝트는 우리의 굿즈를 제작합니다.
+제작된 상품은 크라우드 펀딩의 리워드로
+활용되거나, 스마트스토어를 통해 판매되며,
+스마트스토어의 판매수익 전액은
+가이드런프로젝트의 운영을 위해 사용됩니다.`,
   },
 ]
 
