0f410503bcd620bff023315b64aa2acb4545f167
Pridesd <whwotjr98@naver.com>
2026-01-31 22:40:01 +0900

    This version of Antigravity is no longer supported. Please update to receive the latest features!

--- src/domains/blog/list/category-filters.tsx
+++ src/domains/blog/list/category-filters.tsx
@@ -51,13 +51,15 @@ export const CategoryFilters = ({
   currentCategory = "all",
 }: CategoryFiltersProps) => {
   return (
-    <div id="_category" className="flex items-center gap-2 md:px-10">
+    <div
+      id="_category"
+      className="scrollbar-hide flex items-center gap-2 overflow-y-auto md:px-10">
       {CATEGORIES.map((category) => (
         <Link
           key={category.key}
           aria-pressed={category.key === currentCategory}
           href={`/blog${category.path}/#_category`}
-          className={`flex items-center justify-center rounded-full px-3.5 py-2 hover:bg-black hover:text-white ${category.key === currentCategory ? "bg-black text-white" : "bg-gray-200 text-gray-700"}`}>
+          className={`flex items-center justify-center rounded-full px-3.5 py-2 whitespace-nowrap hover:bg-black hover:text-white ${category.key === currentCategory ? "bg-black text-white" : "bg-gray-200 text-gray-700"}`}>
           {category.label}
           <HiddenText>게시글만 필터링하기</HiddenText>
         </Link>
